73b2d1a891f1ce5ab26fca6f42b41814161050f7
zenDev-2 <cfk102@gmail.com>
2023-08-09 19:21:44 -0400

    integrated modal

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@
 // *
 
 const root = document.querySelector(":root");
-const fireworkColors = ["var(--red)", "var(--blue)", "var(--white)"];
+let fireworkColors = ["var(--red)", "var(--blue)", "var(--white)"];
 const startButton = document.querySelector(".start-button");
 const buttonText = document.querySelector(".button-text");
 const uiModal = document.querySelector(".ui-modal");
@@ -28,32 +28,29 @@ const lchr2 = document.querySelector(".lchr-2");
 const lchr3 = document.querySelector(".lchr-3");
 const launchers = [lchr1, lchr2, lchr3];
 
+// randomizer boolean
+let randomize = false;
+
 // *
-// * main function
+// * main functions
 // *
 
 function fireworkPositionsAndColors() {
-  // randomizes color order
-  const colorset = [];
-  while (colorset.length < 3) {
-    let randomColorIndex = Math.floor(Math.random() * fireworkColors.length);
-    if (!colorset.includes(fireworkColors[randomColorIndex])) {
-      colorset.push(fireworkColors[randomColorIndex]);
-    }
-  }
-
   // spark coloring
   for (let i in fireworkSparks) {
     fireworkSparks[i].map((el) => {
-      el.style.backgroundColor = colorset[i];
-      el.style.filter = `drop-shadow(0 0 0.4em ${colorset[i]})`;
+      el.style.backgroundColor = fireworkColors[i];
+      el.style.filter = `drop-shadow(0 0 0.4em ${fireworkColors[i]})`;
     });
   }
 
   // launcher coloring
   for (let i in launchers) {
-    launchers[i].style.setProperty("--dropShadow-color", `${colorset[i]}`);
-    launchers[i].style.setProperty("--boxShadow-color", `${colorset[i]}`);
+    launchers[i].style.setProperty(
+      "--dropShadow-color",
+      `${fireworkColors[i]}`
+    );
+    launchers[i].style.setProperty("--boxShadow-color", `${fireworkColors[i]}`);
   }
 
   // determines offsetTop value for root css variables
@@ -65,11 +62,23 @@ function fireworkPositionsAndColors() {
   }
 }
 
+// Shuffles fireworkColors array (Fisher-Yates algorithm)
+function randomizeColors() {
+  for (let i = 2; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [fireworkColors[i], fireworkColors[j]] = [
+      fireworkColors[j],
+      fireworkColors[i],
+    ];
+  }
+}
+
+// Interval kicked off by initial click
 function startInterval() {
   setInterval(() => {
-    console.log("swapping colors");
+    if (randomize) randomizeColors();
     fireworkPositionsAndColors();
-  }, 2600);
+  }, 2590);
 }
 
 // starts firework display on click
@@ -77,7 +86,6 @@ let initialClick = false;
 startButton.addEventListener("click", () => {
   if (!initialClick) {
     initialClick = true;
-    // initiates interval to randomize colors
     startInterval();
     // transitions button content + positioning
     buttonText.style.opacity = 0;
@@ -101,5 +109,40 @@ startButton.addEventListener("click", () => {
   }
 });
 
+// *
+// * Display Settings
+// *
+
+// Radius Slider
+const slider = document.querySelector(".slider");
+const output = document.querySelector(".slider-value");
+output.innerHTML = slider.defaultValue;
+slider.value = slider.defaultValue;
+slider.oninput = function () {
+  output.innerHTML = this.value;
+  root.style.setProperty("--explosion-radius", `${slider.value}px`);
+};
+
+// Colorpickers
+const colorpicker = Array.from(document.querySelectorAll(".color-picker"));
+colorpicker.map((el) => {
+  el.onchange = function () {
+    fireworkColors.splice(parseInt(this.id), 1, this.value);
+  };
+});
+
+// Randomizer Checkbox
+const checkbox = document.querySelector("#randomize");
+checkbox.addEventListener("click", () => {
+  if (checkbox.value === "false") {
+    // initiates interval to randomize colors
+    checkbox.value = "true";
+    randomize = true;
+  } else if (checkbox.value === "true") {
+    checkbox.value = "false";
+    randomize = false;
+  }
+});
+
 fireworkPositionsAndColors();
-// this laptop browser viewport is 787px high, for reference
+startButton.style.opacity = 1;
